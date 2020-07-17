6d5e4d1278dd3f5cceee19e9cc2535e9253e66bf
UnfallenSkyMW <usawangyu@hotmail.com>
2020-07-17 14:36:08 -0400

    1.Make login page done, 2.complete medication model 3.add images to display medication 4. display medication details

---
+++ PatientMedicationManagement/Models/MedicationModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace PatientMedicationManagement.Models
+{
+    public class MedicationModel
+    {
+        public string Name { get; set; }
+        public string Id { get; set; }
+        public string Description { get; set; }
+        public string ImgPath { get; set; }
+        public string VoicePath { get; set; }
+        public int UserId { get; set; }
+
+    }
+}
